ffec2dee9eee963b314b15637d9d3b2794da401e
Leo <tu-correo@ejemplo.com>
2026-05-02 03:10:48 -0500

    fix: update n8n api key

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
  */
 
 const N8N_BASE_URL = 'https://n8n.yaperocallate.com'; 
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjYTRkNTE5Zi02OWE2LTQwNGYtYTllNi04MGM0Y2E3MTdlOTAiLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwianRpIjoiNjg2OGI5N2YtNTczOC00NzU0LThlYzUtYzE0ZjNjNDcxNGQ2IiwiaWF0IjoxNzc3NjkzMzg3fQ.YAcP3jt8_Tdc_Csw8ap-SPrdTmcpoqwUC1tSk6MGtDk';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjYTRkNTE5Zi02OWE2LTQwNGYtYTllNi04MGM0Y2E3MTdlOTAiLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwianRpIjoiZDQ4YWMwZTEtYzVmOC00ZThjLTk4YTgtM2FkY2JjMGMxYWY1IiwiaWF0IjoxNzc3NzA5MzgzfQ.ZwJGBzyyZayi3wRA4Rke1JHxPSZWnqjt93E7R5e3pFA';
 
 /**
  * --------------------------------------------------------------------------
